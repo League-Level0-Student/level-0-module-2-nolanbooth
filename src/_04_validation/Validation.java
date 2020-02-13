a5ed101b2388b529c7be194d9bd7338c5c6c31ad
League Student <league@iMac-16.attlocal.net>
2020-02-12 16:55:27 -0800

    it was fun
    yay

--- src/_04_validation/Validation.java
+++ src/_04_validation/Validation.java
@@ -9,17 +9,33 @@ import javax.swing.JOptionPane;
 
 public class Validation {
 	public static void main(String[] args) {
+
+		for (int i = 0; i < 50.; i++) {
+			
 		
 		Random randomMaker = new Random();
-		
+
 		int randomNumber = randomMaker.nextInt(5);
-		
+
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-JOptionPane.showMessageDialog(null, "You are a good person.")
 		// 2. Repeat all the code above 10 times
-		
-		// 3. Find someone to test out your program. They will like it :)
+		if (randomNumber == 0) {
+			JOptionPane.showMessageDialog(null, "You are a good person.");
+		}
+		if (randomNumber == 1) {
+			JOptionPane.showMessageDialog(null, "You are nice.");
+		}
+		if (randomNumber == 2) {
+			JOptionPane.showMessageDialog(null, "You are good at certain things.");
+		}
+		if (randomNumber == 3) {
+			JOptionPane.showMessageDialog(null, "I don't know what to say, so you are nice, I guess.");
+		}
+		if (randomNumber == 4) {
+			JOptionPane.showMessageDialog(null, "I am running out of things to say, so you are... Um... I don't know.");
+		}}
+		// 3. Find someone to test out your program. They will like it >:(
 	}
 }
--- src/_05_magic_8_ball/Magic8Ball.java
+++ src/_05_magic_8_ball/Magic8Ball.java
@@ -6,16 +6,21 @@ package _05_magic_8_ball;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below….
-
+public static void main(String[] args) {
+	Random randomThing = new Random();
+	
+	int randomNumber = randomThing.nextInt();
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
 
 	// 3. Print out this variable
-
+ 
 	// 4. Get the user to enter a question for the 8 ball
-
+JOptionPane.showInputDialog(null , "Put a question into the 'magic' 8 Ball. If you put in the question quickly, you won't have to wait so long");
 	// 5. If the random number is 0
 
 	// -- tell the user "Yes"
@@ -31,5 +36,5 @@ public class Magic8Ball {
 	// 8. If the random number is 3
 
 	// -- write your own answer
-
+}
 }
